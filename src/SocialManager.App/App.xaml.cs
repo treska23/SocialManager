a3cb4afd111dee6faab30f4e4d3297dc3233a238
treska23 <treska23@hotmail.com>
2026-08-25 15:32:27 +0200

    Load saved application theme on startup

--- src/SocialManager.App/App.xaml.cs
+++ src/SocialManager.App/App.xaml.cs
@@ -1,7 +1,13 @@
 using System.Windows;
+using SocialManager.App.Infrastructure;
 
 namespace SocialManager.App;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        ThemeManager.Initialize();
+        base.OnStartup(e);
+    }
 }
